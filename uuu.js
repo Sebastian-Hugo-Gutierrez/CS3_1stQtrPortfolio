13a29ee90075cb394638989242e7378f4251738a
Sebastian Hugo Gutierrez <b2029hsgutierrez@pshs.edu.ph>
2025-09-08 14:30:18 +0800

    first commit

---
+++ uuu.js
@@ -0,0 +1,29 @@
+console.log(1)
+console.log(2)
+console.log(3)
+
+for (let i = 2; i <= 6; i+=2){
+    console.log(1)
+}
+for (let i = 1; i <= 3; i++){
+    console.log(1)
+}
+i = 1;
+while (i <= 3) {
+    console.log(1)
+    i++
+}
+do {
+    console.log(1)
+    i++
+} while (i <= 3);
+
+grades = [70, 80, 90, 100]
+for (let i = 0; i < grades.length; i++){
+    console.log(grades[1])
+}
+console.log(grades[2])
+
+for (var grade of grades) {
+    console.log(grades)
+}
